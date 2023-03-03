6a5e6971b273f93a10a2cfceb519c8e970d7b404
makoviisergii <sergiimakovii78@gmail.com>
2023-03-03 10:22:03 +0200

    3.03

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,23 +1,27 @@
 import styled from "styled-components";
 
-const FriendListItem = (props) => {
+const FriendListItem = ({data}) => {
     return (
 <Item>
   <Status />
-  < Avatar src={props.avatar} alt="User avatar"  />
-  <Name></Name>
+  < Avatar src={data.avatar} alt="User avatar"  />
+  <Name>{data.name}</Name>
 </Item>
     )
 }
 
 const Item = styled.li`
+    background-color: #ffffff;
     display: flex;
     align-items: center;
     justify-content: start;
-    outline: 1px solid tomato;
+    gap: 25px;
     border-radius: 5px;
     height: 100px;
     width: 350px;
+    box-shadow: 0px 5px 9px 0px rgba(0,0,0,0.75);
+-webkit-box-shadow: 0px 5px 9px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 0px 5px 9px 0px rgba(0,0,0,0.75);
 `
 const Status = styled.span`
     margin-left: 20px;
@@ -30,7 +34,8 @@ const Avatar = styled.img`
     width: 48px;
 `
 const Name = styled.p`
-    
+    font-size: 32px;
+    font-weight: 500;
 `
 
 
